42ca2be086f3ccc56c6fcf053b8332e477218837
jdlawrence <jamil.lawrence@gmail.com>
2015-10-15 17:31:28 -0700

    Need to figure out bug that causes me to be $.25 off for values more than 500,000

--- IA.js
+++ IA.js
@@ -1,35 +1,48 @@
-var testAmount = 38242.54;
+var testAmount = 1000000;
 
 function calculate(income, taxObj){
   var level = 0;
   var federalTax = [
+  {bracket: 0, rate: 0.0},
   {bracket: 9225, rate: 0.10},
   {bracket: 37450, rate: 0.15},
   {bracket: 90750, rate: 0.25},
   {bracket: 189300, rate: 0.28},
   {bracket: 411500, rate: 0.33},
   {bracket: 413200, rate: 0.35},
-  {bracket: 413201, rate: 0.396}
+  {bracket: 413200, rate: 0.396}
   ];
   function taxOwed(tax, starting, leftOver, level) {
+    var amounts = [];
     while (leftOver >= 0) {
       // If starting amount is greater than bracket, add entire amount to tax,
       // and subtract bracket from leftOver
-      console.log('tax', tax);
-      if (starting > federalTax[level].bracket) {
-        console.log('inside if');
+      if (level === 0) {
+        level++;
+      }
+      else if (level === 7) {
+        amounts.push(leftOver * federalTax[level].rate);
+        tax += leftOver * federalTax[level].rate;
+        leftOver -= (federalTax[level].bracket - federalTax[level-1].bracket);
+        level++; 
+        break;
+      }
 
-        tax += federalTax[level].bracket * federalTax[level].rate;
-        leftOver -= federalTax[level].bracket;
+      else if (starting > federalTax[level].bracket) {
+        amounts.push((federalTax[level].bracket - federalTax[level-1].bracket) * federalTax[level].rate);
+        tax += (federalTax[level].bracket - federalTax[level-1].bracket) * federalTax[level].rate;
+        leftOver -= (federalTax[level].bracket - federalTax[level-1].bracket);
         level++;
       }
       else {
+        amounts.push(leftOver * federalTax[level].rate);
         tax += leftOver * federalTax[level].rate;
-        leftOver -= federalTax[level].bracket;
+        leftOver -= (federalTax[level].bracket - federalTax[level-1].bracket);
         level++; 
       }
+      // console.log('starting', starting, 'tax', tax, 'leftOver', leftOver, 'level', level);
+      console.log('amounts:', amounts);
 
-      console.log('tax2', tax);
     }
 
     return tax;
@@ -39,4 +52,4 @@ function calculate(income, taxObj){
   return taxOwed(0, income, income, 0);
 }
 
-console.log('Your income is: ', calculate(testAmount));
+console.log('Your tax owed is: ', calculate(testAmount));
