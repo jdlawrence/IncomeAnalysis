200fa9879e07c9da169010229a37acb26a0a493d
jdlawrence <jamil.lawrence@gmail.com>
2015-12-15 15:24:37 -0800

    Tax calculation is now correct but needs to be displayed

--- public/app.js
+++ public/app.js
@@ -16,27 +16,29 @@ app.controller('incomeController', ['$scope', 'Calculate', function($scope, Calc
   $scope.taxableIncome2 = $scope.income2 - $scope.numExemptions2 * exemption; 
   $scope.usableIncome2 = 0;
 
-  var federalTax = [
-  // {bracket: 0, rate: 0.0},
-  // {bracket: 9225, rate: 0.10},
-  // {bracket: 37450, rate: 0.15},
-  // {bracket: 90750, rate: 0.25},
-  // {bracket: 189300, rate: 0.28},
-  // {bracket: 411500, rate: 0.33},
-  // {bracket: 413200, rate: 0.35},
-  // {bracket: 413200, rate: 0.396}
+  var californiaTax = [
   {bracket: 0, rate: 0.0, difference: 0},
-{bracket: 7850, rate: 0.01, difference: 7850},
-{bracket: 18610, rate: 0.02, difference: 10760},
-{bracket: 29372, rate: 0.04, difference: 10762},
-{bracket: 40773, rate: 0.06, difference: 11361},
-{bracket: 51530, rate: 0.08, difference: 10757},
-{bracket: 262222, rate: 0.093, difference: 210692},
-{bracket: 315866, rate: 0.103, difference: 53644},
-{bracket: 526443, rate: 0.113, difference: 210577},
-{bracket: 1000000, rate: 0.123, difference: 473557},
-{bracket: 1000000, rate: 0.133, difference: 0}
+  {bracket: 7850, rate: 0.01, difference: 7850},
+  {bracket: 18610, rate: 0.02, difference: 10760},
+  {bracket: 29372, rate: 0.04, difference: 10762},
+  {bracket: 40773, rate: 0.06, difference: 11361},
+  {bracket: 51530, rate: 0.08, difference: 10757},
+  {bracket: 262222, rate: 0.093, difference: 210692},
+  {bracket: 315866, rate: 0.103, difference: 53644},
+  {bracket: 526443, rate: 0.113, difference: 210577},
+  {bracket: 1000000, rate: 0.123, difference: 473557},
+  {bracket: 1000000, rate: 0.133, difference: 0}
+  ];
 
+  var federalTax = [
+  {bracket: 0, rate: 0.0, difference: 0},
+  {bracket: 9225, rate: 0.10, difference: 9225},
+  {bracket: 37450, rate: 0.15, difference: 28225},
+  {bracket: 90750, rate: 0.25, difference: 53300},
+  {bracket: 189300, rate: 0.28, difference: 98550},
+  {bracket: 411500, rate: 0.33, difference: 222200},
+  {bracket: 413200, rate: 0.35, difference: 1700},
+  {bracket: 413200, rate: 0.396, difference: 0}
   ];
   
   // 1st city expenses
@@ -49,7 +51,8 @@ app.controller('incomeController', ['$scope', 'Calculate', function($scope, Calc
     carInsurance: 1500,
     medical: 1000,
     retirementRate: 10,
-    fedTax: federalTax
+    fedTax: federalTax,
+    stateTax: undefined
   };
 
   // 2nd city expenses
@@ -62,7 +65,8 @@ app.controller('incomeController', ['$scope', 'Calculate', function($scope, Calc
     carInsurance: 1500,
     medical: 1000,
     retirementRate: 10,
-    fedTax: federalTax
+    fedTax: federalTax,
+    stateTax: californiaTax
   };
 
   $scope.updateIncome = function(){
@@ -80,8 +84,8 @@ app.controller('incomeController', ['$scope', 'Calculate', function($scope, Calc
     - $scope.tax;
 
     $scope.taxableIncome2 = $scope.income2 - $scope.numExemptions2 * exemption - standardDeduction;
-    console.log('tax2 is ',Calculate.calculateTax2($scope.taxableIncome2, $scope.expenses2));
     $scope.tax2 = Calculate.calculateTax2($scope.taxableIncome2, $scope.expenses2.fedTax);
+    $scope.stateTax2 = Calculate.calculateTax2($scope.taxableIncome2, $scope.expenses2.stateTax);
     $scope.usableIncome2 = $scope.income2 - $scope.tax2;
     $scope.disposableIncome2 = $scope.income2 
     - $scope.expenses2.medical
@@ -91,7 +95,8 @@ app.controller('incomeController', ['$scope', 'Calculate', function($scope, Calc
     - $scope.expenses2.housing * 12
     - $scope.expenses2.bills
     - $scope.expenses2.food * 365
-    - $scope.tax2;
+    - $scope.tax2
+    - $scope.stateTax2;
   };
   
 }]);
